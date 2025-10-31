d1af52f4e35174c0d1094a3da290d7ec686cd0ef
David Yacoub <daveshtine2010@yahoo.com>
2025-10-31 18:38:57 +0100

    Change base URL in Vite config for GitHub Pages
    Update base URL for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: { outDir: 'docs' } 
+  base: '/fanana-art/',  // because your site lives at ...github.io/fanana-art/
+  build: { outDir: 'docs' } // build into /docs for Pages
 })
